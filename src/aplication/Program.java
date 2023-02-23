c6485d94442aadc6ba1bae9b598fb0d8d2d1d567
João Victor <joaoavilaperasol@hotmail.com>
2023-02-23 17:00:49 -0300

    findById implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,25 +1,18 @@
 package aplication;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller se = new Seller(25, "Joao", "joao@hotmail.com", LocalDate.parse("08/03/2001", fmt), 4500.0);
-		System.out.println(se);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,7 +1,7 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable {
@@ -11,13 +11,15 @@ public class Seller implements Serializable {
 	private Integer id;
 	private String name;
 	private String email;
-	private LocalDate birthDate;
+	private Date birthDate;
 	private Double baseSalary;
+	
+	private Department department;
 	
 	public Seller() {
 	}
 	
-	public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -49,12 +51,21 @@ public class Seller implements Serializable {
 		this.email = email;
 	}
 
-	public LocalDate getBirthDate() {
+	public Date getBirthDate() {
 		return birthDate;
 	}
 
-	public void setBirthDate(LocalDate birthDate) {
+	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 
 	public Double getBaseSalary() {
@@ -84,8 +95,8 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + "]";
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", department="
+				+ department + ", baseSalary=" + baseSalary + "]";
 	}
 	
 	
