d128d8610e8c99a27259ee911a965e8675bab5a9
João Victor <joaoavilaperasol@hotmail.com>
2023-02-23 16:17:49 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,22 @@
 package aplication;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
-
+		
+		Seller se = new Seller(25, "Joao", "joao@hotmail.com", LocalDate.parse("08/03/2001", fmt), 4500.0);
+		System.out.println(se);
 	}
 
 }
